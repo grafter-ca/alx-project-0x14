dec40b3210393f2cbc79326267bd97adba6d574c
habyarimana caleb <callebhabyar55@gmail.com>
2025-08-08 02:00:31 +0200

    updates all component to include jsx

--- alx-movie-app/components/layout/Layout.tsx
+++ alx-movie-app/components/layout/Layout.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import Header from "./Header";
+import Footer from "./Footer";
+import { ComponentProps } from "@/interfaces";
 
-const Layout:React.FC = () => {
+const Layout: React.FC<ComponentProps> = ({ children }) => {
   return (
-    <div>Layout</div>
+    <>
+      <Header />
+      <main className="min-h-screen">{children}</main>
+      <Footer />
+    </>
   )
 }
 
-export default Layout
+export default Layout;
